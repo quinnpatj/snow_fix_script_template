0b09cffc4532164e1a739774abed737b72e04bae
Patrick Quinn <quinnpatj@gmail.com>
2022-11-14 14:18:17 -0600

    New script for mdb_sam_sw_install cleanup

---
+++ snow_fs_template-delete_ci_filtered.js
@@ -0,0 +1,13 @@
+deleteInstall();
+
+function deleteInstall(){
+	var grInstall = new GlideRecord('cmdb_sam_sw_install');
+	// Find all Software Install records created by SCCM, JAMF, or ILMT
+	grInstall.addEncodedQuery('sys_created_byLIKESCCM^ORdiscovery_source=SCCM^ORsccm_group_idISNOTEMPTY^ORdiscovery_source=JAMF^ORdiscovery_source=ILMT');
+	grInstall.query();
+	gs.info("***INFO: Record count being deleted from cmdb_sam_sw_install table: " + gr.getRowCount());
+	// Bypass business rules and workflows
+	grInstall.setWorkflow(false);
+	// Delete all records in the query result
+	grInstall.deleteMultiple();
+}

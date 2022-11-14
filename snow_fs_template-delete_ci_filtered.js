248cb027d711bcc1dad3fe69704d96d44cf3ff00
Patrick Quinn <quinnpatj@gmail.com>
2022-11-14 16:26:41 -0600

    Corrected variable typo

--- snow_fs_template-delete_ci_filtered.js
+++ snow_fs_template-delete_ci_filtered.js
@@ -5,7 +5,7 @@ function deleteInstall(){
 	// Find all Software Install records created by SCCM, JAMF, or ILMT
 	grInstall.addEncodedQuery('sys_created_byLIKESCCM^ORdiscovery_source=SCCM^ORsccm_group_idISNOTEMPTY^ORdiscovery_source=JAMF^ORdiscovery_source=ILMT');
 	grInstall.query();
-	gs.info("***INFO: Record count being deleted from cmdb_sam_sw_install table: " + gr.getRowCount());
+	gs.info("***INFO: Record count being deleted from cmdb_sam_sw_install table: " + grInstall.getRowCount());
 	// Bypass business rules and workflows
 	grInstall.setWorkflow(false);
 	// Delete all records in the query result
